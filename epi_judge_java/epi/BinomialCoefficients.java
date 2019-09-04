053f39d292c13bf5cb45ec3ba7ead34c824f7561
cindyli007 <findingnemo2000@gmail.com>
2019-09-03 23:34:25 -0700

    Add more code

--- epi_judge_java/epi/BinomialCoefficients.java
+++ epi_judge_java/epi/BinomialCoefficients.java
@@ -5,20 +5,6 @@ import epi.test_framework.GenericTest;
 
 // Time: O(N*K), Space: O(N*K)
 public class BinomialCoefficients {
-  @EpiTest(testfile = "binomial_coefficients.tsv")
-
-  public static int computeBinomialCoefficient(int n, int k) {
-    int[] dp = new int[k+1];
-    dp[0]=1;
-    for (int i=1; i<=n; i++) {
-      // NOTICE: must by from high-idx to low-idx, because we use prev-row value from left to right, need guarantee prev
-      // value is not overwritten by this round
-      for (int j=Math.min(i, k); j>0; j--) {
-        dp[j] += dp[j-1];
-      }
-    }
-    return dp[k];
-  }
 
   private static int recursive(int n, int k, int[][] dp) {
     if (n==k || k==0) {
@@ -28,6 +14,26 @@ public class BinomialCoefficients {
       dp[n][k] = recursive(n - 1, k - 1, dp) + recursive(n - 1, k, dp);
     }
     return dp[n][k];
+  }
+
+  @EpiTest(testfile = "binomial_coefficients.tsv")
+  public static int computeBinomialCoefficient(int n, int k) {
+    int[] dp = new int[k+1];
+//    int[][] dp = new int[k+1][n+1];
+//    for (int i=0; i<=n; i++) dp[0][i] = 1; // from i numbers, choose 0, always 1;
+//    for (int i=1; i<=k; i++) dp[i][i] = 1; // from i numbers, choose i, always 1;
+    for (int i=0; i<=n; i++) {
+      int prev = dp[0];
+      dp[0]=1;
+      for (int j=1; j<=Math.min(k, i); j++) {
+        int temp = dp[j];
+        dp[j] += prev;
+        prev = temp;
+//        dp[j][i] = dp[j][i-1] + dp[j-1][i-1];
+      }
+    }
+//    return dp[k][n];
+    return dp[k];
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/IsStringInMatrix.java
+++ epi_judge_java/epi/IsStringInMatrix.java
@@ -6,31 +6,39 @@ import epi.test_framework.GenericTest;
 import java.util.List;
 
 public class IsStringInMatrix {
+  static int N, row, col;
 
   // Time: O(N*M*s), s is the length of pattern, Space: O(N*M*s)
   @EpiTest(testfile = "is_string_in_matrix.tsv")
   public static boolean isPatternContainedInGrid(List<List<Integer>> grid,
                                                  List<Integer> pattern) {
-    int N=pattern.size(), row=grid.size(), col=grid.get(0).size();
-    // dp is store the combination of [x, y] in grid and index in pattern, if dp value is true, means this combo has been visited
+    N=pattern.size();
+    row=grid.size();
+    col=grid.get(0).size();
+    // dp store the combination of [x, y] in grid and index in pattern, if dp value is true, means this combo has been visited
     // and could not match the pattern
     boolean[][][] dp = new boolean[N][row][col];
+    boolean [][] visited = new boolean[row][col];
     for (int i=0; i<row; i++) {
       for (int j=0; j<col; j++) {
-        if (dfs(grid, i, j, pattern, 0, dp)) return true;
+        if (dfs(grid, i, j, pattern, 0, dp, visited)) return true;
       }
     }
     return false;
   }
 
-  private static boolean dfs(List<List<Integer>> grid, int i, int j, List<Integer> pattern, int idx, boolean[][][] dp) {
-    int row = grid.size(), col = grid.get(0).size(), N=pattern.size();
-    if (idx==N) return true; // run out the string, get a match
-    if (i<0 || j<0 || i==row || j==col || dp[idx][i][j] || pattern.get(idx) != grid.get(i).get(j)) return false;
-    boolean res = dfs(grid, i-1, j, pattern, idx+1, dp) || dfs(grid, i, j-1, pattern, idx+1, dp)
-                  || dfs(grid, i+1, j, pattern, idx+1, dp) || dfs(grid, i, j+1, pattern, idx+1, dp);
+  private static boolean dfs(List<List<Integer>> grid, int i, int j, List<Integer> pattern, int idx, boolean[][][] dp, boolean[][] visited) {
+    if (idx==N)
+      return true; // run out the string, get a match
+    if (i<0 || j<0 || i==row || j==col || dp[idx][i][j] || pattern.get(idx) != grid.get(i).get(j) || visited[i][j])
+      return false;
+
+    visited[i][j]=true;
+    boolean res = dfs(grid, i-1, j, pattern, idx+1, dp, visited) || dfs(grid, i, j-1, pattern, idx+1, dp, visited)
+                  || dfs(grid, i+1, j, pattern, idx+1, dp, visited) || dfs(grid, i, j+1, pattern, idx+1, dp, visited);
     if (res) return true;
     dp[idx][i][j]=true;
+    visited[i][j]=false;
     return false;
   }
 
