97358c0d60f3c8412011f6804159174621bc8256
cindyli007 <accountsall007@gmail.com>
2019-09-29 16:22:29 -0700

    Merge branch 'master' of https://github.com/cindyLi007/EPIJudge
    # Conflicts:
    #	epi_judge_java/epi/IsStringDecomposableIntoWords.java
    #	epi_judge_java/epi/IsStringInMatrix.java

--- epi_judge_java/epi/MinimumWaitingTime.java
+++ epi_judge_java/epi/MinimumWaitingTime.java
@@ -10,13 +10,12 @@ public class MinimumWaitingTime {
   @EpiTest(testfile = "minimum_waiting_time.tsv")
 
   public static int minimumTotalWaitingTime(List<Integer> serviceTimes) {
-    // 1, 2, 3, 5
     int res=0, sum=0;
 
     Collections.sort(serviceTimes);
-    for (int i = 1; i < serviceTimes.size(); i++) {
+    for (int i = 0; i < serviceTimes.size()-1; i++) {
       // sum keeps sum of [0, i-1]
-      sum += serviceTimes.get(i - 1);
+      sum += serviceTimes.get(i);
       res += sum;
     }
     return res;
