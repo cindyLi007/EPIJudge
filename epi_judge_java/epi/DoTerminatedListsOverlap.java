616af6d912b19fa56199ed1693f541101a6020d9
cindyli007 <accountsall007@gmail.com>
2019-10-15 23:34:21 -0700

    Merge branch 'master' of https://github.com/cindyLi007/EPIJudge
    # Conflicts:
    #	epi_judge_java/epi/IsStringDecomposableIntoWords.java
    #	epi_judge_java/epi/IsStringInMatrix.java

--- epi_judge_java/epi/DoTerminatedListsOverlap.java
+++ epi_judge_java/epi/DoTerminatedListsOverlap.java
@@ -10,14 +10,46 @@ public class DoTerminatedListsOverlap {
   // Time: O(N) N is the total nodes in both lists
   public static ListNode<Integer>
   overlappingNoCycleLists(ListNode<Integer> l0, ListNode<Integer> l1) {
-    ListNode<Integer> p0 = l0, p1 = l1;
-    while (p0 != p1) {
-      if (p0==null) p0 = l1;
-      else p0 = p0.next;
-      if (p1==null) p1 = l0;
-      else p1 = p1.next;
+    // compute len1 and len2
+    int len0 = length(l0), len1 = length(l1);
+
+    // advance long list
+    while (len1 > len0) {
+      l1 = l1.next;
+      len1--;
     }
-    return p0;
+    while (len1 < len0) {
+      l0 = l0.next;
+      len0--;
+    }
+
+    // traverse both lists and compare nodes, if find same node, return
+    while (l1 != l0) {
+      l1 = l1.next;
+      l0 = l0.next;
+    }
+    return l1;
+  }
+
+  // 用两个指针走两遍list 将两个list首尾串起来，如果一长一短但有common nodes必然会相交
+  public static ListNode<Integer>
+  overlappingNoCycleLists_tricky(ListNode<Integer> l0, ListNode<Integer> l1) {
+    ListNode<Integer> pa = l0, pb = l1;
+    while (pa!=pb) {
+      pa = pa == null ? l1 : pa.next;
+      pb = pb == null ? l0 : pb.next;
+    }
+    return pa;
+  }
+
+  // Java pass by value, so if we change the reference of the object, will not change it outside this method
+  private static int length(ListNode<Integer> listNode) {
+    int count = 0;
+    while (listNode!=null) {
+      count++;
+      listNode = listNode.next;
+    }
+    return count;
   }
 
   @EpiTest(testfile = "do_terminated_lists_overlap.tsv")
